110829fddda7c67c612f6c5656d889680a2b6a3d
Taras1326 <166243411+Taras1326@users.noreply.github.com>
2026-04-04 00:07:23 +0300

    hww-1.react

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import './App.css';
+import "./App.css";
 
 function App() {
   const name = "Taras";
 
   const site = {
     title: "Google",
-    url: "https://www.google.com/"
+    url: "https://www.google.com/",
   };
 
   const a = 7;
@@ -20,8 +20,11 @@ function App() {
 
       <p>Ласкаво просимо до нашого сайту!</p>
 
-      <img width="700"
-        src={"https://pictures.tribuna.com/image/6cff39e2-97b5-441d-8441-6c8dca6f92b9?width=1920&quality=70"}
+      <img
+        width="700"
+        src={
+          "https://pictures.tribuna.com/image/6cff39e2-97b5-441d-8441-6c8dca6f92b9?width=1920&quality=70"
+        }
         alt={"Random image"}
       />
 
